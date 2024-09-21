6c3df902ba3517f624bb599d0cf8a888457be94b
344_09 <344_09@344-09>
2024-09-21 14:58:06 +0700

    Add project files.

---
+++ ExchangeRate 4024    4/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace ExchangeRate_4024____4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.picyuan = new System.Windows.Forms.PictureBox();
+            this.piceuro = new System.Windows.Forms.PictureBox();
+            this.picdollar = new System.Windows.Forms.PictureBox();
+            this.picyen = new System.Windows.Forms.PictureBox();
+            this.picpond = new System.Windows.Forms.PictureBox();
+            this.tbath = new System.Windows.Forms.TextBox();
+            this.ldonla = new System.Windows.Forms.Label();
+            this.lapond = new System.Windows.Forms.Label();
+            this.ladollar = new System.Windows.Forms.Label();
+            this.laeuro = new System.Windows.Forms.Label();
+            this.layuan = new System.Windows.Forms.Label();
+            this.layen = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picyuan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piceuro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picdollar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picyen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picpond)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(211, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "อัตราเเลกเปลี่ยนเงินตรา";
+            // 
+            // picyuan
+            // 
+            this.picyuan.BackColor = System.Drawing.Color.Transparent;
+            this.picyuan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picyuan.Image = global::ExchangeRate_4024____4.Properties.Resources.CNY;
+            this.picyuan.Location = new System.Drawing.Point(12, 331);
+            this.picyuan.Name = "picyuan";
+            this.picyuan.Size = new System.Drawing.Size(83, 83);
+            this.picyuan.TabIndex = 4;
+            this.picyuan.TabStop = false;
+            this.picyuan.MouseLeave += new System.EventHandler(this.picyuan_MouseLeave);
+            this.picyuan.MouseHover += new System.EventHandler(this.picyuan_MouseHover);
+            // 
+            // piceuro
+            // 
+            this.piceuro.BackColor = System.Drawing.Color.Transparent;
+            this.piceuro.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.piceuro.Image = global::ExchangeRate_4024____4.Properties.Resources.EUR;
+            this.piceuro.Location = new System.Drawing.Point(12, 242);
+            this.piceuro.Name = "piceuro";
+            this.piceuro.Size = new System.Drawing.Size(83, 83);
+            this.piceuro.TabIndex = 3;
+            this.piceuro.TabStop = false;
+            this.piceuro.Click += new System.EventHandler(this.pictureBox4_Click);
+            this.piceuro.MouseLeave += new System.EventHandler(this.piceuro_MouseLeave);
+            this.piceuro.MouseHover += new System.EventHandler(this.piceuro_MouseHover);
+            // 
+            // picdollar
+            // 
+            this.picdollar.BackColor = System.Drawing.Color.Transparent;
+            this.picdollar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picdollar.Image = global::ExchangeRate_4024____4.Properties.Resources.USD;
+            this.picdollar.Location = new System.Drawing.Point(12, 64);
+            this.picdollar.Name = "picdollar";
+            this.picdollar.Size = new System.Drawing.Size(83, 83);
+            this.picdollar.TabIndex = 2;
+            this.picdollar.TabStop = false;
+            this.picdollar.MouseLeave += new System.EventHandler(this.picdollar_MouseLeave);
+            this.picdollar.MouseHover += new System.EventHandler(this.picdollar_MouseHover);
+            // 
+            // picyen
+            // 
+            this.picyen.BackColor = System.Drawing.Color.Transparent;
+            this.picyen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picyen.Image = global::ExchangeRate_4024____4.Properties.Resources.JPY;
+            this.picyen.Location = new System.Drawing.Point(12, 420);
+            this.picyen.Name = "picyen";
+            this.picyen.Size = new System.Drawing.Size(83, 83);
+            this.picyen.TabIndex = 1;
+            this.picyen.TabStop = false;
+            this.picyen.MouseLeave += new System.EventHandler(this.picyen_MouseLeave);
+            this.picyen.MouseHover += new System.EventHandler(this.picyen_MouseHover);
+            // 
+            // picpond
+            // 
+            this.picpond.BackColor = System.Drawing.Color.Transparent;
+            this.picpond.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.picpond.Image = global::ExchangeRate_4024____4.Properties.Resources.GBP;
+            this.picpond.Location = new System.Drawing.Point(12, 153);
+            this.picpond.Name = "picpond";
+            this.picpond.Size = new System.Drawing.Size(83, 83);
+            this.picpond.TabIndex = 0;
+            this.picpond.TabStop = false;
+            this.picpond.MouseLeave += new System.EventHandler(this.picpond_MouseLeave);
+            this.picpond.MouseHover += new System.EventHandler(this.picpond_MouseHover);
+            // 
+            // tbath
+            // 
+            this.tbath.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tbath.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbath.Location = new System.Drawing.Point(403, 107);
+            this.tbath.Name = "tbath";
+            this.tbath.Size = new System.Drawing.Size(100, 29);
+            this.tbath.TabIndex = 0;
+            this.tbath.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // ldonla
+            // 
+            this.ldonla.AutoSize = true;
+            this.ldonla.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ldonla.Location = new System.Drawing.Point(207, 122);
+            this.ldonla.Name = "ldonla";
+            this.ldonla.Size = new System.Drawing.Size(0, 25);
+            this.ldonla.TabIndex = 11;
+            // 
+            // lapond
+            // 
+            this.lapond.AutoSize = true;
+            this.lapond.BackColor = System.Drawing.Color.Transparent;
+            this.lapond.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lapond.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lapond.Location = new System.Drawing.Point(101, 194);
+            this.lapond.Name = "lapond";
+            this.lapond.Size = new System.Drawing.Size(178, 42);
+            this.lapond.TabIndex = 13;
+            this.lapond.Text = "................";
+            // 
+            // ladollar
+            // 
+            this.ladollar.AutoSize = true;
+            this.ladollar.BackColor = System.Drawing.Color.Transparent;
+            this.ladollar.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ladollar.ForeColor = System.Drawing.SystemColors.Control;
+            this.ladollar.Location = new System.Drawing.Point(101, 107);
+            this.ladollar.Name = "ladollar";
+            this.ladollar.Size = new System.Drawing.Size(178, 42);
+            this.ladollar.TabIndex = 14;
+            this.ladollar.Text = "................";
+            // 
+            // laeuro
+            // 
+            this.laeuro.AutoSize = true;
+            this.laeuro.BackColor = System.Drawing.Color.Transparent;
+            this.laeuro.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.laeuro.ForeColor = System.Drawing.SystemColors.Control;
+            this.laeuro.Location = new System.Drawing.Point(101, 283);
+            this.laeuro.Name = "laeuro";
+            this.laeuro.Size = new System.Drawing.Size(178, 42);
+            this.laeuro.TabIndex = 15;
+            this.laeuro.Text = "................";
+            // 
+            // layuan
+            // 
+            this.layuan.AutoSize = true;
+            this.layuan.BackColor = System.Drawing.Color.Transparent;
+            this.layuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.layuan.ForeColor = System.Drawing.SystemColors.Control;
+            this.layuan.Location = new System.Drawing.Point(101, 372);
+            this.layuan.Name = "layuan";
+            this.layuan.Size = new System.Drawing.Size(178, 42);
+            this.layuan.TabIndex = 16;
+            this.layuan.Text = "................";
+            // 
+            // layen
+            // 
+            this.layen.AutoSize = true;
+            this.layen.BackColor = System.Drawing.Color.Transparent;
+            this.layen.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.layen.ForeColor = System.Drawing.SystemColors.Control;
+            this.layen.Location = new System.Drawing.Point(101, 461);
+            this.layen.Name = "layen";
+            this.layen.Size = new System.Drawing.Size(178, 42);
+            this.layen.TabIndex = 17;
+            this.layen.Text = "................";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::ExchangeRate_4024____4.Properties.Resources._32232321;
+            this.pictureBox1.Location = new System.Drawing.Point(330, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 246);
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::ExchangeRate_4024____4.Properties.Resources.e185527661d588ac2ba84bce88665ae2;
+            this.ClientSize = new System.Drawing.Size(677, 541);
+            this.Controls.Add(this.tbath);
+            this.Controls.Add(this.layen);
+            this.Controls.Add(this.layuan);
+            this.Controls.Add(this.laeuro);
+            this.Controls.Add(this.ladollar);
+            this.Controls.Add(this.lapond);
+            this.Controls.Add(this.ldonla);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picyuan);
+            this.Controls.Add(this.piceuro);
+            this.Controls.Add(this.picdollar);
+            this.Controls.Add(this.picyen);
+            this.Controls.Add(this.picpond);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picyuan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.piceuro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picdollar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picyen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picpond)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picpond;
+        private System.Windows.Forms.PictureBox picyen;
+        private System.Windows.Forms.PictureBox picdollar;
+        private System.Windows.Forms.PictureBox piceuro;
+        private System.Windows.Forms.PictureBox picyuan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbath;
+        private System.Windows.Forms.Label ldonla;
+        private System.Windows.Forms.Label lapond;
+        private System.Windows.Forms.Label ladollar;
+        private System.Windows.Forms.Label laeuro;
+        private System.Windows.Forms.Label layuan;
+        private System.Windows.Forms.Label layen;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ExchangeRate 4024    4/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExchangeRate_4024____4
+{
+    public partial class Form1 : Form
+    {
+        double bath;
+            double dollar = 36.3791;
+            double pond = 46.2969;
+            double euro = 39.1320;
+            double yuan = 4.9638;
+            double yen = 22.9279;
+        public Form1()
+
+        {
+            
+
+            InitializeComponent();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picdollar_MouseHover(object sender, EventArgs e)
+        {
+            if(tbath.Text   !="   ")
+            bath    =   double.Parse(tbath.Text);
+            double result = bath / dollar;
+            ladollar.Text = result.ToString("#.###");
+        }
+
+        private void picdollar_MouseLeave(object sender, EventArgs e)
+        {
+            ladollar.Text = "................";
+        }
+
+        private void picpond_MouseHover(object sender, EventArgs e)
+        {
+            if (tbath.Text != "   ")
+                bath = double.Parse(tbath.Text);
+            double result = bath / pond;
+            lapond.Text = result.ToString("#.###");
+        }
+
+        private void picpond_MouseLeave(object sender, EventArgs e)
+        {
+            lapond.Text = "................";
+        }
+
+        private void piceuro_MouseHover(object sender, EventArgs e)
+        {
+            if (tbath.Text != "   ")
+                bath = double.Parse(tbath.Text);
+            double result = bath / euro;
+            laeuro.Text = result.ToString("#.###");
+        }
+
+        private void piceuro_MouseLeave(object sender, EventArgs e)
+        {
+            laeuro.Text = "................";
+        }
+
+        private void picyuan_MouseHover(object sender, EventArgs e)
+        {
+            if (tbath.Text != "   ")
+                bath = double.Parse(tbath.Text);
+            double result = bath / yuan;
+            layuan.Text = result.ToString("#.###");
+        }
+
+        private void picyuan_MouseLeave(object sender, EventArgs e)
+        {
+            layuan.Text = "................";
+        }
+
+        private void picyen_MouseHover(object sender, EventArgs e)
+        {
+
+            if (tbath.Text != "   ")
+                bath = double.Parse(tbath.Text);
+            double result = bath / yen;
+            layen.Text = result.ToString("#.###");
+        }
+
+        private void picyen_MouseLeave(object sender, EventArgs e)
+        {
+            layen.Text = "................";
+        }
+    }
+}
